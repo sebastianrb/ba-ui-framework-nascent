73b09cb4b82a58910bd3774c596ac73380c3df59
Denine Guy <denineguy@gmail.com>
2017-03-05 07:46:10 -0500

    fixed padding of dropdown updated dropdown to close after selection

--- javascript/dropdowns.js
+++ javascript/dropdowns.js
@@ -2,6 +2,7 @@
   var select = document.getElementById('select');
   var ul;
   var li;
+  var button;
   document.addEventListener('click', function(e) {
     if(e.target.nodeName === 'BUTTON') {
       ul = e.target.nextElementSibling;
@@ -9,19 +10,27 @@
     }
 
     if (e.target.classList.contains('dropdown__item')) {
-        e.target.parentNode.childNodes.forEach(function (node)  {
+      ul = e.target.parentNode;
+      li = e.target.parentNode.childNodes;
+        li.forEach(function (node)  {
             if (node.classList && node.classList.contains('dropdown__item--selected')) {
                 node.classList.remove('dropdown__item--selected');
             }
         });
         e.target.classList.add('dropdown__item--selected');
+        ul.classList.toggle('dropdown__menu-show');
     }
 
     if(e.target.parentNode.id === 'select' &&  e.target.nodeName === 'LI') {
-        var li = e.target;
-        var selection = e.target.innerText;
-        var button = e.target.parentNode.previousElementSibling;
+        var li;
+        var selection;
+        var button;
+        ul.classList.toggle('dropdown__menu-show');
+        li = e.target;
+        selection = e.target.innerText;
+        button = e.target.parentNode.previousElementSibling;
         button.firstChild.textContent = selection;
+        ul.classList.toggle('dropdown__menu-show');
     }
     e.preventDefault();
     e.stopPropagation();
