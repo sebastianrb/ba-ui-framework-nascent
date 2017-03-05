247a48df17cd737557f9bf10b0bc81c2dc199001
Denine Guy <denineguy@gmail.com>
2017-03-05 08:09:38 -0500

    added notes to javascript and added update dropdown to close if clicked anywhere other than body

--- javascript/dropdowns.js
+++ javascript/dropdowns.js
@@ -4,11 +4,13 @@
   var li;
   var button;
   document.addEventListener('click', function(e) {
+    // check if button is clicked and toggle showing dropdown menu
     if(e.target.nodeName === 'BUTTON') {
       ul = e.target.nextElementSibling;
       ul.classList.toggle('dropdown__menu-show');
     }
 
+    // check if dropdown item is a selected add class to seleted item
     if (e.target.classList.contains('dropdown__item')) {
       ul = e.target.parentNode;
       li = e.target.parentNode.childNodes;
@@ -21,6 +23,7 @@
         ul.classList.toggle('dropdown__menu-show');
     }
 
+    // check if dropdown is a select dropdown and append selection name to button.
     if(e.target.parentNode.id === 'select' &&  e.target.nodeName === 'LI') {
         var li;
         var selection;
@@ -32,6 +35,12 @@
         button.firstChild.textContent = selection;
         ul.classList.toggle('dropdown__menu-show');
     }
+
+    // check if clicked anywhere on the body close the dropdown.
+    if(e.target.classList.length === 0) {
+      ul.setAttribute('class', 'dropdown__menu');
+    };
+
     e.preventDefault();
     e.stopPropagation();
   });
